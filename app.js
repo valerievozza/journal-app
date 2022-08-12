deca0e547d15b2a7df0f41fc8b9ecd48ed5498d3
valerievozza <valerievozza@gmail.com>
2022-08-12 16:16:59 -0700

    user model and passport google strategy

--- app.js
+++ app.js
@@ -3,10 +3,15 @@ const express = require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
 const exphbs = require('express-handlebars')
+const passport = require('passport')
+const session = require('express-session')
 const connectDB = require('./config/db')
 
 // Load config
 dotenv.config({ path: './config/config.env' })
+
+// Passport config
+require('./config/passport.env')(passport)
 
 connectDB()
 
@@ -23,6 +28,17 @@ app.engine('.hbs', exphbs.engine({
   extname: '.hbs'
 }))
 app.set('view engine', '.hbs')
+
+// Sessions
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false, // Don't save a session if nothing is modified
+  saveUninitialized: true, // Don't create a session until something is stored
+}))
+
+// Passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
 
 // Static folder
 app.use(express.static(path.join(__dirname, 'public')))
