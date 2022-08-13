135b7db0993237699ce2b952c26831405f438a21
valerievozza <valerievozza@gmail.com>
2022-08-13 10:04:21 -0700

    added entry model and error page templates

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const router = express.Router()
 const { ensureAuth, ensureGuest } = require('../middleware/auth')
 
+const Entry = require('../models/Entry')
+
 // @desc    Landing page
 // @route   GET /
 router.get('/', (req, res) => {
@@ -18,9 +20,16 @@ router.get('/login', ensureGuest, (req, res) => {
 
 // @desc    User profile page
 // @route   GET /profile
-router.get('/profile', ensureAuth, (req, res) => {
-  console.log(req.user)
-  res.render('profile')
+router.get('/profile', ensureAuth, async (req, res) => {
+  try {
+    const entries = await Entry.find({ user: req.user.id }).lean()
+    res.render('profile', {
+      name: req.user.firstName,
+      entries
+    })
+  } catch (err) {
+    console.error(err)
+  }
 })
 
 module.exports = router
