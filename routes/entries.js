815bd646f71b88bed28339a362bc5236f6c2470f
valerievozza <valerievozza@gmail.com>
2022-08-15 10:02:48 -0700

    working app!

--- routes/entries.js
+++ routes/entries.js
@@ -41,44 +41,110 @@ router.get('/', ensureAuth, async (req, res) => {
   }
 })
 
+// @desc    Show single entry
+// @route   GET /entries/:id
+router.get('/:id', ensureAuth, async (req, res) => {
+  try {
+    let entry = await Entry.findById(req.params.id)
+      .populate('user')
+      .lean()
+
+    if (!entry) {
+      return res.render('/error/404')
+    }
+
+    res.render('entries/show', {
+      entry
+    })
+  } catch (err) {
+    console.error(err)
+    res.render('error/404')
+  }
+})
+
 // @desc    Show edit page
 // @route   GET /entries/edit/:id
 router.get('/edit/:id', ensureAuth, async (req, res) => {
-  const entry = await Entry.findOne({
-    _id: req.params.id
-  }).lean()
-
-  if (!entry) {
-    return res.render('error/404')
-  }
-
-  if (entry.user != req.user.id) {
-    res.redirect('/entries')
-  } else {
-    res.render('entries/edit', {
-      entry,
-    })
+  try {
+    const entry = await Entry.findOne({
+      _id: req.params.id
+    }).lean()
+  
+    if (!entry) {
+      return res.render('error/404')
+    }
+  
+    if (entry.user != req.user.id) {
+      res.redirect('/entries')
+    } else {
+      res.render('entries/edit', {
+        entry,
+      })
+    }
+  } catch (err) {
+    console.error(err)
+    res.render('error/500')
   }
 })
 
 // @desc    Update entry
 // @route   PUT /entries/:id
 router.put('/:id', ensureAuth, async (req, res) => {
-  let entry = await Entry.findById(req.params.id).lean()
+  try {
+    let entry = await Entry.findById(req.params.id).lean()
 
-  if (!entry) {
-    return res.render('error/404')
+    if (!entry) {
+      return res.render('error/404')
+    }
+
+    if (entry.user != req.user.id) {
+      res.redirect('/entries')
+    } else {
+      entry = await Entry.findOneAndUpdate({ _id: req.params.id }, req.body, {
+        new: true,
+        runValidators: true
+      })
+
+      res.redirect('/journal')
+    }
+  } catch (err) {
+    console.error(err)
+    res.render('error/500')
   }
+})
 
-  if (entry.user != req.user.id) {
-    res.redirect('/entries')
-  } else {
-    entry = await Entry.findOneAndUpdate({ _id: req.params.id }, req.body, {
-      new: true,
-      runValidators: true
-    })
-
+// @desc    Delete entry
+// @route   DELETE /entries/:id
+router.delete('/:id', ensureAuth, async (req, res) => {
+  try {
+    await Entry.remove({ _id: req.params.id })
     res.redirect('/journal')
+  } catch (err) {
+    console.error(err)
+    res.render('error/500')
+  }
+})
+
+// @desc    User entries
+// @route   GET /entries/user/:userId
+
+// CHANGE THIS TO RENDER USER JOURNAL PAGE
+router.get('/user/:userId', ensureAuth, async (req, res) => {
+  try {
+    const entries = await Entry.find({
+      user: req.params.userId,
+      status: 'public',
+    })
+      .populate('user')
+      .sort({ createdAt: 'desc' })
+      .lean()
+
+    res.render('entries/index', {
+      entries,
+    })
+  } catch (err) {
+    console.error(err)
+    res.render('error/500')
   }
 })
 
